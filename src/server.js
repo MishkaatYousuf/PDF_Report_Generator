d2804c8d6634c3ef0fe3f9080bba7c5286d6f75f
MishkaatYousuf <mish9758@gmail.com>
2026-09-04 13:46:19 +0500

    Stage 0: setup ready

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
